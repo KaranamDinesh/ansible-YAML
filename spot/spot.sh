d87f31649681807a189757d9d1440f4949b9da4b
KaranamDinesh <71174249+KaranamDinesh@users.noreply.github.com>
2020-10-25 12:40:09 +0530

    sample program

--- spot/spot.sh
+++ spot/spot.sh
@@ -2,6 +2,5 @@
 
 for i in *.json; do
   echo $i
-  aws ec2 request-spot-fleet --spot-fleet-request-config file://frontend.json
   aws ec2 request-spot-fleet --spot-fleet-request-config file://$i
 done
