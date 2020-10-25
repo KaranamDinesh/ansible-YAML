9725534ad188481f53def77eddc0b82b7529b29e
KaranamDinesh <71174249+KaranamDinesh@users.noreply.github.com>
2020-10-25 09:57:33 +0530

    sample program

--- spot/generate-hosts.sh
+++ spot/generate-hosts.sh
@@ -6,4 +6,4 @@ for i in *.json ; do
   COMPONENT=$(echo $i | sed -e 's/.json//')
   IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
   echo $IP component=$COMPONENT >>/tmp/hosts
-done
+  done
