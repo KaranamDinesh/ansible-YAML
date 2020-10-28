57c5deb96597e06f1478e292faad7b6b10907141
KaranamDinesh <71174249+KaranamDinesh@users.noreply.github.com>
2020-10-28 10:28:59 +0530

    sample

--- spot/generate-hosts.sh
+++ spot/generate-hosts.sh
@@ -7,5 +7,5 @@ for i in *.json ; do
   IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
   echo $IP component=$COMPONENT >>/tmp/hosts
   sed -e "s/IPADDRESS/${IP}/" -e "s/DNS_NAME/${COMPONENT}/" record > /tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id Z0150647140RRZE9VT30N --change-batch file:///tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id Z01196811XGLCEKVA665C --change-batch file:///tmp/record.json
 done
