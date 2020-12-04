3c886a28b5ad34e9de8a444a54b8bbbe4b143826
KaranamDinesh <71174249+KaranamDinesh@users.noreply.github.com>
2020-12-04 11:38:00 +0530

    sample

--- spot/generate-hosts.sh
+++ spot/generate-hosts.sh
@@ -8,6 +8,6 @@ for i in *.json ; do
   echo $IP component=$COMPONENT >>/tmp/hosts
   sed -e "s/IPADDRESS/${IP}/" -e "s/DNS_NAME/${COMPONENT}/" record > /tmp/record.json
 
-  aws route53 change-resource-record-sets --hosted-zone-id Z063448232JYR64KQPU29 --change-batch file:///tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id Z07191062WZNK89H4T3UK --change-batch file:///tmp/record.json
 
 done
