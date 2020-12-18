b511ced36aff05639edbbea76e9f7b2a83adb231
KaranamDinesh <71174249+KaranamDinesh@users.noreply.github.com>
2020-12-18 08:57:07 +0530

    sample

--- spot/spot.sh
+++ spot/spot.sh
@@ -3,9 +3,9 @@ for i in *.json; do
 echo $i
 aws ec2 request-spot-fleet --spot-fleet-request-config file://$i
 done
-if [ -z $1 ]; then
-  echo " Provide input json file"
-  exit 1
-fi
+#if [ -z $1 ]; then
+  #echo " Provide input json file"
+  #exit 1
+#fi
 
-aws ec2 request-spot-fleet --spot-fleet-request-config file://$1
+#aws ec2 request-spot-fleet --spot-fleet-request-config file://$1
