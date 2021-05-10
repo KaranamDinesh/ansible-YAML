319b3265634462b7a6d4a429cd9a154805d3ff57
KaranamDinesh <71174249+KaranamDinesh@users.noreply.github.com>
2021-05-10 12:44:49 +0530

    sample

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -1,8 +1,12 @@
 resource "null_resource" "nothing" {
-  //depends_on = [aws_instance.sample]
+  depends_on = [aws_instance.sample]
 }
 
 resource "aws_instance" "sample" {
+  count = 2
   ami = "ami-079a3f3cf00741286"
   instance_type = "t2.micro"
+}
+provider "aws" {
+  region = "us-east-1"
 }
