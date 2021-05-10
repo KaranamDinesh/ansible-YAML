fc25b80341bf90652473a84e612f3f86ae8074df
KaranamDinesh <71174249+KaranamDinesh@users.noreply.github.com>
2021-05-10 17:00:18 +0530

    sample

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -6,6 +6,9 @@ resource "aws_instance" "sample" {
   count = 2
   ami = "ami-079a3f3cf00741286"
   instance_type = "t2.micro"
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 provider "aws" {
   region = "us-east-1"
